f2b462031f3867928c0527897f6cf784410b67e0
minahil <minahilrehmat00@gmail.com>
2025-12-31 21:13:10 +0500

    Remove functionality to signup from login page

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -286,12 +286,7 @@ export default function LoginPage() {
                             </span>
                         </motion.button>
 
-                        <p className="mt-8 text-center text-slate-400">
-                            Don't have an account?{' '}
-                            <Link to="/signup" className="font-bold text-indigo-400 hover:text-indigo-300 transition-colors">
-                                Sign Up
-                            </Link>
-                        </p>
+
                     </motion.form>
 
                 </motion.div>
